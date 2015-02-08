f5af625d600e22a8b55965e22a16c5e89e07361e
mmouterde <martin.mouterde@gmail.com>
2015-02-08 15:29:27 +0100

    - fix #12

---
+++ firefox/lib/main.js
@@ -0,0 +1,11 @@
+var pageMod = require("sdk/page-mod");
+var data = require("sdk/self").data;
+var cssContent = data.load('override.css');
+pageMod.PageMod({
+  include: "*",
+  attachTo: ["existing", "top"],
+  contentScriptFile: [data.url("jquery-1.7.1.min.js"),data.url("FlexyTrello.js")],
+  contentScriptOptions: {
+    cssContent: cssContent,
+  }
+});
