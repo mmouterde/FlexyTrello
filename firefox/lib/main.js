701ad3d02dfd195db27a969786e863aeb5e4b8ab
mmouterde <martin.mouterde@gmail.com>
2015-02-09 22:16:28 +0100

    - fix FFext URL matcher

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -2,10 +2,10 @@ var pageMod = require("sdk/page-mod");
 var data = require("sdk/self").data;
 var cssContent = data.load('override.css');
 pageMod.PageMod({
-  include: "https://www.trello.com/b/*",
+  include: "https://trello.com/b/*",
   attachTo: ["existing", "top"],
   contentScriptFile: [data.url("jquery-1.7.1.min.js"),data.url("FlexyTrello.js")],
   contentScriptOptions: {
-    cssContent: cssContent,
+    cssContent: cssContent
   }
 });
